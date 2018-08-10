f5a7f61f11d46020ace25eb101993f16c6058a3c
guanjun li <793515697@qq.com>
2018-08-10 18:48:23 +0800

    搭建工程结构

--- BaseTools/BaseTools/BaseTools.h
+++ BaseTools/BaseTools/BaseTools.h
@@ -2,7 +2,7 @@
 //  BaseTools.h
 //  BaseTools
 //
-//  Created by zsy on 2018/8/10.
+//  Created by liguanjun on 2018/8/10.
 //  Copyright © 2018年 liguanjun. All rights reserved.
 //
 
---
+++ BaseTools/CBaseTools/CBaseTools.h
@@ -0,0 +1,13 @@
+//
+//  CBaseTools.h
+//  CBaseTools
+//
+//  Created by liguanjun on 2018/8/10.
+//  Copyright © 2018年 liguanjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBaseTools : NSObject
+
+@end
