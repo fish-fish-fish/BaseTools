4dcd82054ded745560ea5ddf0f65eeeb68635b10
guanjun li <793515697@qq.com>
2018-08-10 18:42:35 +0800

    new BaseTools.framework Project

---
+++ BaseTools/BaseTools/BaseTools.h
@@ -0,0 +1,19 @@
+//
+//  BaseTools.h
+//  BaseTools
+//
+//  Created by zsy on 2018/8/10.
+//  Copyright © 2018年 liguanjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaseTools.
+FOUNDATION_EXPORT double BaseToolsVersionNumber;
+
+//! Project version string for BaseTools.
+FOUNDATION_EXPORT const unsigned char BaseToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseTools/PublicHeader.h>
+
+
